ee95d72d062546571d18646d413a99db469a8da4
Boris Bovkunovich <yakov.antipov.94@mail.ru>
2025-06-10 00:40:58 +0300

    form

--- Form.js
+++ Form.js
@@ -1,28 +1,39 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const submitButton = document.querySelector(".form__button");
-  let cartShown = true;
-  let cartImage = false;
+const form = document.querySelector(".form");
+const container = document.querySelector(".container");
 
-  submitButton.addEventListener("click", function () {
-    if (cartShown) {
-      cartImage = document.createElement("img");
-      cartImage.src =
-        "https://support.songtradr.com/hc/article_attachments/115010619108";
-      cartImage.style.maxWidth = "20%";
+// Создаем элемент изображения (изначально скрыт)
+const img = document.createElement("img");
+img.src = "https://support.songtradr.com/hc/article_attachments/115010619108";
+img.alt = "Success image";
+img.style.display = "none";
+img.style.maxWidth = "100%";
+img.style.marginTop = "20px";
+img.style.borderRadius = "8px";
+img.style.cursor = "pointer";
 
-      const form = document.querySelector("form");
-      form.appendChild(cartImage);
+// Добавляем изображение в контейнер
+container.appendChild(img);
 
-      cartShown = false;
-      submitButton.value = "Remove Image";
-    } else {
-      if (cartImage) {
-        cartImage.remove();
-        cartImage = true;
-      }
+// Обработчик отправки формы
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
 
-      cartShown = true;
-      submitButton.value = "submit";
-    }
-  });
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  if (email && password) {
+    // Показываем изображение
+    img.style.display = "block";
+
+    // Очищаем форму
+    form.reset();
+
+    // Прокручиваем к изображению
+    img.scrollIntoView({ behavior: "smooth" });
+  }
+});
+
+// Обработчик клика по изображению (удаление)
+img.addEventListener("click", function () {
+  img.style.display = "none";
 });
